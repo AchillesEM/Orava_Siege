88ca01c24cf60db672a43dfe5fd45227c5eeaa93
Elchrys <elliecinuk@gmail.com>
2023-05-11 13:03:44 +0100

    ViewpointCamera made
    -Game Manager changes
    -new Script CameraLookAtSeperateScreen
    -UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,7 +20,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameState == "playable")
+        {
+            print("kablamm");
+        }
     }
 
     public void PlayButton()
